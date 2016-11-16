b90e822c5f3dc786d5cffe0394bcb29b2e27a0b5
Heiko Faller <heiko_faller@t-online.de>
2016-11-16 15:23:45 +0100

    Berrechnung der Anzahl von Oft, Lang und ort eingefügt

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@
 var filterArray = new Array;
 
 var numberHowOften = [0,0,0,0,0,0];
+var numberHowLong = [0,0,0,0];
+var numberWhere = [0,0,0,0];
+
 var data;
 $.getJSON("data/data.json", function(obj) {
     data = obj;
@@ -37,4 +40,56 @@ function changeVisibilityOfStudyIfNecessary(cb) {
 
 function showResults(){
     var filteredData = filterPoll(data, filterArray);
+    resetNumbers();
+    filteredData.forEach(function(element){
+        if(element[howOften]===window.oft0){
+            numberHowOften[0]++;
+        }
+        if(element[howOften]===window.oft1){
+            numberHowOften[1]++;
+        }
+        if(element[howOften]===window.oft2){
+            numberHowOften[2]++;
+        }
+        if(element[howOften]===window.oft3){
+            numberHowOften[3]++;
+        }
+        if(element[howOften]===window.oft4){
+            numberHowOften[4]++;
+        }
+        if(element[howOften]===window.oft5){
+            numberHowOften[5]++;
+        }
+
+        if(element[howOften]===window.lang0){
+            numberHowLong[0]++;
+        }
+        if(element[howOften]===window.lang1){
+            numberHowLong[1]++;
+        }
+        if(element[howOften]===window.lang2){
+            numberHowLong[2]++;
+        }
+        if(element[howOften]===window.lang3){
+            numberHowLong[3]++;
+        }
+        
+        if(element[howOften]===window.ort0){
+            numberWhere[0]++;
+        }
+        if(element[howOften]===window.ort1){
+            numberWhere[1]++;
+        }
+        if(element[howOften]===window.ort2){
+            numberWhere[2]++;
+        }
+        
+    });
+    console.log(numberHowOften);
+}
+
+function resetNumbers(){
+    numberHowOften = [0,0,0,0,0,0];
+    numberHowLong = [0,0,0,0];
+    numberWhere = [0,0,0,0];
 }
