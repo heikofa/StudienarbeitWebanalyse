2196ec20863a809e76455a7e495696ff22e7c6fc
Pico <micha@piertzik.com>
2016-11-23 15:09:39 +0100

    impl Function excel string

--- js/excelstring.js
+++ js/excelstring.js
@@ -1,3 +1,12 @@
 function arrayOfObjectsToExcelString(array) {
 
+    var TAB = "\t";
+    var mainString = "";
+    array.forEach(function (object) {
+        var objectString = object.caption + TAB +
+            object.absolute + TAB +
+            object.relative + "\n";
+        mainString += objectString;
+    });
+    return mainString;
 }
